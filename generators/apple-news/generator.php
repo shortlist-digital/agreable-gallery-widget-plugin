24eb5a63c4744b93444d731e6d8dbf92576ff5da
ecoad <elliot.coad@shortlist.com>
2017-11-13 17:41:30 +0000

    Apple News progress

--- generators/apple-news/generator.php
+++ generators/apple-news/generator.php
@@ -4,19 +4,19 @@ namespace Agreable\Widgets\Gallery\Generators\AppleNews;
 use \Timber;
 
 class Generator {
-  public function get(array $widget) {
+	public function get(array $widget) {
+		$images = $widget['gallery_items'];
 
-    $images = $widget['gallery_items'];
+		$json_as_string = Timber::compile(
+			'./template.json.twig',
+			array(
+				'images' => $images,
+				'crop' => $widget['crop']
+			)
+		);
 
-    $json_as_string = Timber::compile(
-      './template.json.twig',
-      array(
-        'images' => $images
-      )
-    );
+		$json_as_object = json_decode($json_as_string);
 
-    $json_as_object = json_decode($json_as_string);
-
-    return $json_as_object;
-  }
+		return $json_as_object;
+	}
 }
